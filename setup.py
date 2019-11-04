917295f0275fd4b51404b9c08b04c3e31d547b90
AbstractMonkey <sean.hobin@gmail.com>
2019-11-04 18:02:08 -0500

    aw yeah

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_AbstractMonkey",
-    version="0.0.1",
+    version="0.0.2",
     author="Sean Hobin",
     description="A collection of Data Science helper functions",
     long_description="Useful functions for data science",
